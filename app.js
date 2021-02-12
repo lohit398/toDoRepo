f3e2845574de8d1b5d3ca85123ded37db36330a3
Lohit <lohitkrishna39@gmail.com>
2021-02-12 12:26:32 +0530

    dynamic port - process env variables -  app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 const path = require('path');
+const port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/'));
+
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/home.html'));
 });
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log('Example app listening on port 3000!');
 });
